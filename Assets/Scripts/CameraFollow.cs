5856709ad643a0c0c431bd68115ab4db470b832f
Lolster24Vito <vitomir.hardi@gmail.com>
2022-01-20 20:58:37 +0100

    Created a new project and added basic movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Range(0.01f,1.5f)][SerializeField] private float _SmoothTime=0.2f;
+    private Transform _player;
+    private Vector2 _currentVelocity;
+    
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 lerpedPosition = Vector2.SmoothDamp(transform.position, _player.position,ref _currentVelocity, _SmoothTime);
+        transform.position = new Vector3(lerpedPosition.x, lerpedPosition.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float _speed=4;
+    private Vector2 _input;
+    private Vector2 _lastInput;
+    private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+    private bool _walking;
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (_input == Vector2.zero)
+        {
+            _walking = false;
+            _animator.SetBool("Walking", _walking);
+        }
+        else { 
+            _walking = true;
+            _animator.SetBool("Walking", _walking);
+        }
+
+        if (_lastInput != _input&&_walking)
+        {
+            _lastInput = _input;
+            _animator.SetFloat("DirectionHorizontal", _lastInput.x);
+            _animator.SetFloat("DirectionVertical", _lastInput.y);
+
+        }
+    }
+    private void FixedUpdate()
+    {
+        _rigidbody2D.MovePosition((Vector2)transform.position+ (_input* _speed*Time.fixedDeltaTime));
+    }
+}
